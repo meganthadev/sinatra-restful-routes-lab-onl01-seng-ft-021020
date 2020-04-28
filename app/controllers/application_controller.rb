1fcedebd6380a676fe58a46d880ba8716452ee97
Megan Horsfield <meganthadev@gmail.com>
2020-04-28 05:56:47 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,21 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  # code actions here!
+  get '/new' do 
+    erb :new 
+  end   
+  
+  get '/show' do 
+    erb :show
+  end   
+  
+  get '/edit' do 
+    erb :edit 
+  end   
+  
+  get '/index' do 
+    @recipes = Recipe.all 
+    erb :index
+  end   
 
 end
